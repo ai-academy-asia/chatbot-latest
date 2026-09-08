2781b532ff34a7f9d30631ff94f3f7af8d0d2a5d
Z <z@Zs-MacBook-Pro.local>
2026-09-08 16:36:42 +0800

    d

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { v4: uuidv4 } = require('uuid')
+const { randomUUID: uuidv4 } = require('node:crypto')
 require('dotenv').config()
 
 const app = express()
